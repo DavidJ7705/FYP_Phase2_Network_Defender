0251e463ede7e1c44a5e82d72cf356198e29a5e2
David <G00419108@atu.ie>
2025-11-27 17:12:11 +0000

    Added script to take sleep action and starting invalid actions

---
+++ Learning/UnderstandingActions/invalid_actions.py
@@ -0,0 +1,3 @@
+#https://cage-challenge.github.io/cage-challenge-4/pages/tutorials/03_Actions/A_Understanding_Actions/4_Invalid_Actions/
+# mac: venv/bin/python -m Learning.UnderstandingActions. ions
+# windows: .\venv\Scripts\python -m Learning.UnderstandingActions.invalid_actions
