20ce8c06fe465caabab016ae8c27d4360d352d95
DavidJ7705 <118185346+DavidJ7705@users.noreply.github.com>
2025-11-28 17:37:10 +0000

    Finished the first blue monitor file, gonna move it to the monitor dir. Copy and pasted that code for monitoring the green one. still need to finish the green oen and writ eup in one note

---
+++ Learning/Blue/Monitor/blue_monitor_green.py
@@ -0,0 +1,45 @@
+#https://cage-challenge.github.io/cage-challenge-4/pages/tutorials/03_Actions/B_Blue_Actions/1_Monitor/
+# mac: venv/bin/python -m Learning.Blue.Monitor.blue_monitor_green
+# windows: .\venv\Scripts\python -m Learning.Blue.Monitor.blue_monitor_green
+
+
+from pprint import pprint
+
+from CybORG import CybORG
+from CybORG.Simulator.Scenarios import EnterpriseScenarioGenerator
+from CybORG.Agents import SleepAgent, FiniteStateRedAgent, EnterpriseGreenAgent
+from CybORG.Simulator.Actions import Monitor
+
+steps = 200
+sg = EnterpriseScenarioGenerator(blue_agent_class = SleepAgent,
+                                green_agent_class = SleepAgent,
+                                red_agent_class = FiniteStateRedAgent,
+                                steps = steps
+                                )
+
+cyborg = CybORG(scenario_generator=sg, seed = 1000)
+blue_agent_name = 'blue_agent_0'
+
+reset = cyborg.reset(agent = blue_agent_name)
+initial_obs = reset.observation
+
+# pprint(initial_obs.keys())
+
+action = Monitor(0, blue_agent_name)
+results = cyborg.step(agent=blue_agent_name, action = action)
+
+step = 1
+base_obs = results.observation
+
+# print(f"step count: {step}")
+# pprint(base_obs)
+
+new_obs = base_obs
+
+while new_obs == base_obs and step < steps:
+    results = cyborg.step(agent = blue_agent_name, action = action)
+    step = step+1
+    new_obs = results.observation
+
+print(f"step count: {step}")
+pprint(new_obs)
