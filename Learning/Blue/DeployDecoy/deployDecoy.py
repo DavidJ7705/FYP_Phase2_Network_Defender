9c11806ccd07de43feee726654a5f1253487b47c
DavidJ7705 <118185346+DavidJ7705@users.noreply.github.com>
2025-12-08 12:13:18 +0000

    Successfully deployed a decoy on

--- Learning/Blue/DeployDecoy/deployDecoy.py
+++ Learning/Blue/DeployDecoy/deployDecoy.py
@@ -2,12 +2,14 @@
 # mac: venv/bin/python -m Learning.Blue.DeployDecoy.deployDecoy
 # windows: .\venv\Scripts\python -m Learning.Blue.DeployDecoy.deployDecoy
 
+from Learning.Blue.Analyse.analyse import blue_agent_name
 from pprint import pprint
 
 from CybORG import CybORG
 from CybORG.Simulator.Scenarios import EnterpriseScenarioGenerator
 from CybORG.Agents.SimpleAgents.ConstantAgent import SleepAgent
 from CybORG.Simulator.Actions.AbstractActions.Analyse import Analyse
+from CybORG.Simulator.Actions.ConcreteActions.DecoyActions.DeployDecoy import DeployDecoy
 from Learning.Blue.DeployDecoy.root_shell_func import cyborg_with_root_shell_on_cns0
 
 cyborg = cyborg_with_root_shell_on_cns0()
@@ -19,3 +21,14 @@ shell_ip = cyborg.environment_controller.state.hostname_ip_map['contractor_netwo
 
 cyborg.environment_controller.state.hosts[target_host].processes = []
 cyborg.environment_controller.state.hosts[target_host].services = {}
+
+action = DeployDecoy(session = 0, agent = blue_agent_name, hostname = target_host)
+action.duration = 1
+
+obs, _, _, _, = cyborg.parallel_step(actions = {blue_agent_name: action})
+
+print("deployed decoy")
+print("Blue:")
+print(obs[blue_agent_name])
+print("\n")
+
