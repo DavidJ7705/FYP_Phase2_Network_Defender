ca30933cac267721077d2aac7c199f54f770b522
David <G00419108@atu.ie>
2026-01-20 11:23:02 +0000

    successfully completed the control traffic tutorial to block and allow traffic on different parts of a network.

---
+++ Learning/Blue/ControlTraffic/allowTraffic.py
@@ -0,0 +1,27 @@
+#https://cage-challenge.github.io/cage-challenge-4/pages/tutorials/03_Actions/B_Blue_Actions/6_Control_Traffic/
+# mac: venv/bin/python -m Learning.Blue.ControlTraffic.allowTraffic
+# windows: .\venv\Scripts\python -m Learning.Blue.ControlTraffic.allowTraffic
+
+from pprint import pprint
+
+from CybORG import CybORG
+from CybORG.Simulator.Scenarios import EnterpriseScenarioGenerator
+from CybORG.Agents import SleepAgent, FiniteStateRedAgent, EnterpriseGreenAgent
+from CybORG.Simulator.Actions.ConcreteActions.ControlTraffic import AllowTrafficZone
+
+steps = 200
+sg = EnterpriseScenarioGenerator(blue_agent_class=SleepAgent, 
+                                green_agent_class=SleepAgent, 
+                                red_agent_class=SleepAgent,
+                                steps=steps)
+cyborg = CybORG(scenario_generator=sg, seed=1000)
+cyborg.reset()
+
+blue_agent_name = 'blue_agent_0'
+action_space = cyborg.get_action_space(blue_agent_name)
+
+action = AllowTrafficZone(session=0, agent=blue_agent_name, from_subnet='restricted_zone_a_subnet', to_subnet='restricted_zone_b_subnet')
+
+results = cyborg.step(agent=blue_agent_name, action=action)
+obs = results.observation
+pprint(obs)
